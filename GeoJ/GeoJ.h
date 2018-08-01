58d9d26db82e1ba295a297f4dcc114e2964cbb62
sina moradbakhti <sina_moradbakhti@yahoo.com>
2018-08-01 19:28:35 +0430

    Initial Commit

---
+++ GeoJ/GeoJ.h
@@ -0,0 +1,19 @@
+//
+//  GeoJ.h
+//  GeoJ
+//
+//  Created by Sina Moradbakhti on 8/1/18.
+//  Copyright © 2018 parcweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoJ.
+FOUNDATION_EXPORT double GeoJVersionNumber;
+
+//! Project version string for GeoJ.
+FOUNDATION_EXPORT const unsigned char GeoJVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoJ/PublicHeader.h>
+
+
